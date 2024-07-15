e3577aa22f34ca889db1c089aad582f9a80e31f7
MariuszJM <mariusz.michna.j@gmail.com>
2024-07-15 10:54:00 +0200

    feat: Clean and sanitize title data before saving to YAML file.

--- data_storage.py
+++ data_storage.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 
 
 class DataStorage:
@@ -15,6 +16,21 @@ class DataStorage:
         for source, titles in data_storage.data.items():
             self.data.setdefault(source, {}).update(titles)
 
+    def clean_title(self, title):
+        clean_title = re.sub(r'[^a-zA-Z0-9\s]', '', title)
+        return clean_title
+
     def save_to_yaml(self, filename):
+        cleaned_data = {}
+        for source, titles in self.data.items():
+            cleaned_data[source] = {self.clean_title(title): tags for title, tags in titles.items()}
         with open(filename, "w") as file:
-            yaml.dump(self.data, file, default_flow_style=False, sort_keys=False)
+            yaml.dump(cleaned_data, file, default_flow_style=False, sort_keys=False)
+
+
+if __name__ == "__main__":
+    data_storage = DataStorage()
+    data_storage.add_data('YouTube', 'Pydantic is all you need: Jason Liu', tag='tutorial')
+    data_storage.add_data('YouTube', '[CHI 2024] &quot;We Need Structured Output&quot;: Towards User-centered Constraints on LLM Output', tag='conference')
+    data_storage.add_data('YouTube', 'JSON Output from Mistral 7B LLM [LangChain, Ctransformers]', tag='example')
+    data_storage.save_to_yaml('output.yaml')
