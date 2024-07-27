bc422df5a9190b853ec326628f0f7195fa7f5990
MariuszJM <mariusz.michna.j@gmail.com>
2024-07-27 14:30:35 +0200

    feat: Improved code organization creating moduls and refactor main.

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import os
 import yaml
 from src.data_storage import DataStorage
-from src.llm.llm_factory import LLMFactory
 from src.utils import load_config, create_output_directory
 from src.processors.processor_factory import ProcessorFactory
 
-if __name__ == "__main__":
+def load_configs():
     execution_config = load_config('./config/execution_config.yaml')
-    sources_per_query = execution_config['sources_per_query']
     user_config = load_config('./config/user_config.yaml')
-    queries = user_config['search_phrases']
-    platforms = [platform.lower() for platform in user_config['platforms']]
-    max_outputs_per_platform = user_config['max_outputs_per_platform']
-    time_horizon = user_config['time_horizon']
-    specific_questions = user_config['specific_questions']
+    return execution_config, user_config
 
-    llm = LLMFactory.create_llm(model_type="ollama", model_name="llama3:instruct")
-    name = llm.provide_run_name(queries, specific_questions)
-    combined_data = DataStorage()
-    comb_data_witout_content = DataStorage()
-    comb_less_relevant_data = DataStorage()
-    comb_rejected_by_relevance = DataStorage()
+def initialize_data_storages():
+    return DataStorage(), DataStorage(), DataStorage(), DataStorage()
 
+def process_platforms(platforms, queries, sources_per_query, specific_questions, time_horizon, max_outputs_per_platform):
+    combined_data, comb_data_witout_content, comb_less_relevant_data, comb_rejected_by_relevance = initialize_data_storages()
     for platform_name in platforms:
         try:
             processor = ProcessorFactory.create_processor(platform_name)
@@ -38,19 +30,39 @@ if __name__ == "__main__":
             comb_rejected_by_relevance.combine(rejected_by_relevance)
         except ValueError as e:
             print(e)
+    return combined_data, comb_data_witout_content, comb_less_relevant_data, comb_rejected_by_relevance
+
+def save_data(output_dir, name, combined_data, filtered, user_config):
+    combined_data.save_to_yaml(os.path.join(output_dir, f"{name}.yaml"))
+    with open(os.path.join(output_dir, f"filtered_data.yaml"), "w") as file:
+        yaml.dump(filtered, file, default_flow_style=False, sort_keys=False)
+    with open(os.path.join(output_dir, f"run_config.yaml"), "w") as file:
+        yaml.dump(user_config, file, default_flow_style=False, sort_keys=False)
+
+def main():
+    execution_config, user_config = load_configs()
+    sources_per_query = execution_config['sources_per_query']
+    queries = user_config['search_phrases']
+    platforms = [platform.lower() for platform in user_config['platforms']]
+    max_outputs_per_platform = user_config['max_outputs_per_platform']
+    time_horizon = user_config['time_horizon']
+    specific_questions = user_config['specific_questions']
+
+    combined_data, comb_data_witout_content, comb_less_relevant_data, comb_rejected_by_relevance = process_platforms(
+        platforms, queries, sources_per_query, specific_questions, time_horizon, max_outputs_per_platform
+    )
 
     output_dir = create_output_directory('runs')
-    combined_data.save_to_yaml(os.path.join(output_dir, f"{name}.yaml"))
+    name = ProcessorFactory.create_processor(platforms[0]).llm.provide_run_name(queries, specific_questions)
     filtered = {
         'data_witout_content': comb_data_witout_content.data,
         'less_relevant_data': comb_less_relevant_data.data,
         'rejected_by_relevance': comb_rejected_by_relevance.data
     }
-    with open(os.path.join(output_dir, f"filtered_data.yaml"), "w") as file:
-        yaml.dump(filtered, file, default_flow_style=False, sort_keys=False)
-    
-    with open(os.path.join(output_dir, f"run_config.yaml"), "w") as file:
-        yaml.dump(user_config, file, default_flow_style=False, sort_keys=False)
-    
+    save_data(output_dir, name, combined_data, filtered, user_config)
+
     print("Combined Data:", combined_data.to_dict())
     print(f"Data saved to: {output_dir}")
+
+if __name__ == "__main__":
+    main()
