0ade4ea16447218734be165a750297ae2f745470
MariuszJM <mariusz.michna.j@gmail.com>
2024-08-09 09:32:43 +0200

    Dirty version for multiple configs done

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import logging
+import os
+import glob
 from src.utils import create_output_directory, load_config, save_data
 from src.processors.process_platforms import process_platforms
 
-output_dir = create_output_directory('runs')
+def setup_logging(output_dir):
+    log_filename = f"{output_dir}/app.log"
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  
 
-log_filename = f"{output_dir}/app.log"
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)  
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    file_handler = logging.FileHandler(log_filename)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
-file_handler = logging.FileHandler(log_filename)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
 
-logger.addHandler(stream_handler)
-logger.addHandler(file_handler)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.ERROR)
 
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.ERROR)
+    return logger
 
-
-def main():
-    config = load_config('./config/config.yaml')
-    logger.debug("Configuration loaded.")
+def process_config_file(config_file, logger):
+    config = load_config(config_file)
+    logger.debug(f"Configuration loaded from {config_file}.")
+    output_dir = create_output_directory(config['output_dir'])
+    setup_logging(output_dir)
+    
     search_phrases = config['search_queries']
     platforms = [platform.lower() for platform in config['platforms']]
     max_outputs = config['max_outputs_per_platform']
@@ -40,5 +45,18 @@ def main():
 
     logger.info("Data saved to: %s", output_dir)
 
+def main(config_path):
+    logger = logging.getLogger('main_logger')
+    
+    if os.path.isdir(config_path):
+        config_files = glob.glob(os.path.join(config_path, '*.yaml'))
+        for config_file in config_files:
+            process_config_file(config_file, logger)
+    elif os.path.isfile(config_path):
+        process_config_file(config_path, logger)
+    else:
+        logger.error(f"Invalid path provided: {config_path}")
+
 if __name__ == "__main__":
-    main()
+    config_path = './config/Quaterly'  
+    main(config_path)
